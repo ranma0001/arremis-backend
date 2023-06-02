92947bf4a13bf44e601fda6a3c3eae885939eb72
unknown <ronnie.chy26@gmail.com>
2023-06-02 15:19:44 +0800

    added dompdf

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -3,32 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Dompdf\Dompdf;
-use Illuminate\Support\Facades\View;
+use PDF;
 
 class PdfController extends Controller
 {
 
     public function generatePDF()
     {
-        // Load the Blade template
-        $template = 'pdf-template.work-order';
-        $dynamicData = [
-            'name' => 'John Doe',
-            'email' => 'john.doe@example.com',
-            'date' => date('Y-m-d'),
-        ];
+        $logo  = public_path() . '/images/da.jpg';
+        view()->share('logo', $logo);
 
-        $htmlContent = View::make($template, $dynamicData)->render();
-        $dompdf = new Dompdf();
-
-        $dompdf->loadHtml($htmlContent);
-
-        $dompdf->setPaper('A4', 'portrait');
-
-        $dompdf->render();
-
-        $dompdf->stream('sample.pdf', ['Attachment' => false]);
+        $pdf = PDF::loadView('pdf-template.work-order');
+        $pdf->setPaper('letter', 'portrait');
+        return $pdf->download('work-order.pdf');
     }
 
 }
--- resources/views/pdf-template/work-order.blade.php
+++ resources/views/pdf-template/work-order.blade.php
@@ -1,11 +1,74 @@
 <!doctype html>
 <html lang="en-US">
-<head>
-<link rel="stylesheet" type="text/css" href="style.css"/>
-</head>
-<body>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+    <style>
+        body{
+            font-family: Helvetica, sans-serif;
+            border : 2px solid #000;
+            padding : 5px;
+        }
 
-    <div>  test   </div>
-    <img alt="test" src="http://127.0.0.1:8000/images/da.jpg"/>
-</body>
+        .image-header {
+            width : 120px;
+            height : 120px;
+            position: absolute;
+            top : 5px;
+            left : 5px;
+        }
+
+        .header-font {
+            font-size: 12px;
+        }
+
+        .header-title{
+            position: absolute;
+            top : 5px;
+            left : 150px;
+        }
+
+    </style>
+    <body>
+        <div style="position:relative">
+            <img src="{{ $logo }}" class="image-header">
+            <div style="line-height: 5px; position : absolute;" class="header-title">
+                <p class="header-font">
+                    Department of Agriculture
+                </p>
+                <p class="header-font">
+                    <b>BUREAU OF AGRICULTURAL AND FISHERIES ENGINEERING</b>
+                </p>
+                <p class="header-font">
+                    Diliman, Quezon City
+                </p>
+                <p class="header-font">
+                    Tel. No. (02) 8294-6452 Fax no.(02)8941-8151
+                </p>
+                <p class="header-font">
+                    Trunk Line(02)8928-8756 to 65 Local 2472
+                </p>
+                <p class="header-font">
+                    Email Add:bafe@da.gov.ph
+                </p>
+            </div>
+            <div style="line-height: 5px; position : absolute; top : 150px; left : 50px;">
+                <b>SRED-ARS-PTOWO-21-02-___</b>
+            </div>
+            <div style="line-height: 5px; position : absolute; top : 220px; left:280px">
+                <p style="font-size: 20px;">
+                    <i><b>WORK ORDER</b></i>
+                </p>
+            </div>
+            <div  style="position : absolute; top : 300px; left:30px">
+                <p style="text-align: justify;">
+                    This is to authorize the bearer/s whose name/s and signature/s appears below, to conduct on-site inspection and evaluation for the registration and issuance
+                    of permit to operate for agricultural and fisheries machinery manufacturers, fabricators, assemblers, distributors, dealers, importers, and exporters
+                    (MFADDIEs).
+                </p>
+            </div>
+
+            
+        </div>
+    </body>
 </html>
